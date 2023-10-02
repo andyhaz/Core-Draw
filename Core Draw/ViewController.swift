f19b19516868cc50bdfe6f0fd267068517d13f42
andrew <andyhaz1970@gmail.com>
2023-10-01 21:39:36 -0500

    Initial Commit

---
+++ Core Draw/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Core Draw
+//
+//  Created by andyhaz on 10/1/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
